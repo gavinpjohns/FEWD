fa94b8ee62daf6042b07b8ea65a3ce357729856c
Gavin Johns <gavinpjohns@gmail.com>
2016-06-14 21:22:00 -0700

    hw

--- code-alongs/color-switcher-3/js/color-switcher-3.js
+++ code-alongs/color-switcher-3/js/color-switcher-3.js
@@ -8,6 +8,9 @@ var textTheme = document.querySelector(".text-theme")
 //-----------------------------------------------
 ul.addEventListener('click', changeTheme);
 
+//on page load, get the theme from localStorage
+window.addEventListener('load', updatePage);
+
 //Event handler functions
 //-----------------------------------------------
 function changeTheme(e) {
@@ -20,4 +23,18 @@ function changeTheme(e) {
 
 	body.className = e.target.className;
 	textTheme.textContent = e.target.className;
+
+	//save theme to localStorage
+	localStorage.setItem("theme", e.target.className);
 }
+
+//get theme from localStorage
+//eg red
+var theme = localStorage.getItem("theme")
+
+function updatePage() {
+	body.className = theme;
+	textTheme.textContent = theme;
+}
+
+
