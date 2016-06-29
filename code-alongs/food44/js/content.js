5d30fd3daccdf5d039fbebcde7b81724ecb0e50f
Gavin Johns <gavinpjohns@gmail.com>
2016-06-28 21:35:46 -0700

    update

---
+++ code-alongs/food44/js/content.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+var ikesPlace = {
+	"name": "Ike's Place",
+	"street_address": "3489 16th St.",
+	"phone": "(415) 553-6888"
+};
+
+var subway = {
+	"name": "Subway",
+	"street_address": "1199 Sutter St.",
+	"phone": "(415) 346-6756"
+};
+
+var smfhCafe = {
+	"name": "SFMH Cafe",     
+	"street_address": "900 Hyde St.",
+	"phone": "(415) 353-6000",
+};
+
+     
+var json = {
+	"objects": [
+		ikesPlace,
+		subway,
+		smfhCafe
+	]
+};
+
